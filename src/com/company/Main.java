0ff7bace1a9b65e8e58858d599b6c6ecb3ab234d
17kidby_m <17kidby_m@bexleygs.co.uk>
2023-01-25 09:59:41 +0000

    e

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,10 +8,14 @@ import java.util.ArrayList;
 public class Main {
 
     public static void main(String[] args) {
+        ArrayList<String> inputList = new ArrayList<>();
         fileHandler file = new fileHandler();
         String user = "";
+        int userInt = 0;
         ArrayList<String> PersistentArrayList = new ArrayList<>();
         Scanner input = new Scanner(System.in);
+        boolean reset = false;
+
 
         System.out.println("Create new file?");
         user = input.nextLine();
@@ -20,10 +24,26 @@ public class Main {
             file.createFile("New_file.txt");
         }
 
-        System.out.println("Would you liek to input or receive from the file");
-        
+        System.out.println("What would you like to do with the file?");
+        System.out.println("[1]: input");
+        System.out.println("[2]: read");
+        userInt = input.nextInt();
+
+        if (userInt == 1){
+            System.out.println("First, would you like to clear the file?");
+            user = input.nextLine();
+            if (user.equals("yes")){
+                reset = true;
+            }
+
+            System.out.println("What would you like to input?");
+            user = input.nextLine();
+            inputList.add(user);
+            file.writeToAdd("New_file.txt", inputList, reset);
+            
 
 
+        }
 
 
 
